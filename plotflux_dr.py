58564facd828bba0b109ba249758fac3df7e1e1a
Yi-Cheng Chuang <ychuang@wm.edu>
2024-07-26 17:05:45 -0400

    Create plotflux_dr.py

---
+++ plotflux_dr.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 26 16:44:40 2024
+
+@author: ychuang
+"""
+
+import os
+import sys
+
+import flux_plot as fp
+import B2pl_method as bm
+
+
+
+
+
+def main(plotall):
+
+    print("Initializing SOLPSxport")
+    xp = fp.SOLPSxport(workdir = os.getcwd())
+
+    print("Getting flux profiles")
+    xp.getSOLPSfluxProfs(plotit= plotall)
+
+
+# --- Launch main() ----------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    import argparse
+
+    py3_9 = (sys.version_info[0] >= 3 and sys.version_info[1] >= 9)
+
+    parser = argparse.ArgumentParser(description='Generate new b2.transport.inputfile files for SOLPS',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('-p', '--plotall', help='plot flux profile', type= bool, default=None)
+    
+    args = parser.parse_args()
+    
+    main(plotall= args.plotall)
+    
